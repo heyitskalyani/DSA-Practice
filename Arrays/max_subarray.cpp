d033184d01cf5d35eeb87a8eb22f2f479ff80532
kalyani2808 <kalyanibunha02@gmail.com>
2026-03-08 23:25:54 +0530

    Added maximum subarray and consecutive ones problems

---
+++ Arrays/max_subarray.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int n = nums.size();
+        double sum = 0;
+
+        for(int i = 0; i < k; i++)
+        {
+            sum += nums[i];
+        }
+
+        double maxSum = sum;
+
+        for(int i = k; i < n; i++)
+        {
+            sum = sum + nums[i] - nums[i-k];
+            if(sum > maxSum)
+            {
+                maxSum = sum;
+            }
+        }
+
+        return maxSum / k;
+    }
+};
--- Arrays/move_zeroes.cpp
+++ Arrays/move_zeroes.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <algorithm>
+using namespace std;
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
