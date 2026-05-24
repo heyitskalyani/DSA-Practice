0ca712cad36c0b1c886cd9f9f3863be766ae47c0
kalyani2808 <kalyanibunha02@gmail.com>
2026-05-24 23:35:26 +0530

    added new problems

---
+++ Arrays/Continuous Subarray Sum.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        
+        unordered_map<int, int> mp;
+
+        int sum = 0;
+
+        mp[0] = -1;
+
+        for(int i = 0; i < nums.size(); i++) {
+
+            sum += nums[i];
+
+            int rem = sum % k;
+
+            if(mp.find(rem) != mp.end()) {
+
+                if(i - mp[rem] >= 2) {
+                    return true;
+                }
+
+            } else {
+                mp[rem] = i;
+            }
+        }
+
+        return false;
+    }
+};
