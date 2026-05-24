7187a39dc24c4000a08cbc38ac7f6e89a37a98cb
kalyani2808 <kalyanibunha02@gmail.com>
2026-05-24 23:26:27 +0530

    Added new problems

---
+++ Arrays/Subarray Sum Equals K.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        
+        unordered_map<int, int> mp;
+
+        int sum = 0;
+        int count = 0;
+
+        mp[0] = 1;
+
+        for(int i = 0; i < nums.size(); i++) {
+
+            sum += nums[i];
+
+            if(mp.find(sum - k) != mp.end()) {
+                count += mp[sum - k];
+            }
+
+            mp[sum]++;
+        }
+
+        return count;
+    }
+};
